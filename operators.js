c85ea1d5a93a5248ed63a864bedea8c4baa96c7a
Dima Ostapchuk <do.company2007@gmail.com>
2024-01-25 16:31:46 +0200

    Опис вашого коміту тут

--- operators.js
+++ operators.js
@@ -13,3 +13,19 @@ console.log(result)
 13 % 2 === 1
 13 % 3 === 1
 14 % 4 === 2
+
+age += 10;
+
+if(true){
+    console.log("inside if")
+} else{
+    console.log("inside else block")
+}
+
+function canDrinkAlcho(age){if(age< 18){
+    return "ти дуже маленький"
+}  else if (age> 100){
+    return " ти застарий, ще можеш померти";
+}else {
+    return "ти можеш все";
+}}
