6d74f5c2d57ab2b54c58680ab201a2919bdbed53
Dima Ostapchuk <do.company2007@gmail.com>
2024-01-25 16:14:02 +0200

    Course

---
+++ operators.js
@@ -0,0 +1,15 @@
+let name = "Dim";
+
+let age = 18;
+age++
+age--
+let weight = 100
+
+//= - / * % 
+let result = age + weight
+console.log(result)
+11 % 2 === 1
+12 % 2 === 0
+13 % 2 === 1
+13 % 3 === 1
+14 % 4 === 2
---
+++ practice.js
@@ -0,0 +1,33 @@
+console.log("Hello World!!")
+console.log(typeof 45);
+console.log(typeof a);
+age = null
+console.log(age)
+
+salary = 1000000
+console.log(typeof salary)
+
+console.log(typeof "Dima has salary " + salary)
+console.log(typeof 'Dima has salary ' + salary)
+
+console.log(typeof BigInt(123))
+
+console.log(typeof {})
+
+console.log(typeof new Object())
+a={name : "Dim", echoAge: function(age){console.log('me age is ' + age)}}
+e.echoAge
+
+{
+    name : "Dim"
+}
+
+a = 10;
+b = 10;
+console.log(a == b)
+
+function call(){
+    let a = 10;
+    a = 12;
+}
+call()
